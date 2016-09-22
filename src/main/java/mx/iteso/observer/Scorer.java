24c9e467e46cfa107aca8ea6ff927e57b0ecde76
StarOfDeath <hectorfernandoruiz@gmail.com>
2016-09-22 07:58:50 -0500

    xx

---
+++ src/main/java/mx/iteso/observer/Scorer.java
@@ -0,0 +1,10 @@
+package mx.iteso.observer;
+
+/**
+ * Created by lamos on 9/22/2016.
+ */
+public class Scorer {
+    public void ScorerGoal(String Name, int Number, String Position, String Team){
+        
+    }
+}
